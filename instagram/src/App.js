676e3c1764bf087db1efd6422969a6e3e4040e74
samjeffcoat <samjeffcoat@gmail.com>
2019-01-18 14:26:05 -0600

    installed reactstrap to project

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import SearchBar from "./components/SearchBar/SearchBar";
 import PostContainer from "./components/PostContainer/PostContainer";
+import { Button } from "reactstrap";
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <SearchBar />
         <PostContainer />
+        <Button color="danger">Danger!</Button>
       </div>
     );
   }
