3fb152e30eb6ee6eb67ab8d3b487ab5ef035da41
samjeffcoat <samjeffcoat@gmail.com>
2019-01-21 17:19:26 -0600

    added input box for adding comment

---
+++ instagram/src/components/CommentSection/CommentInput.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class CommentInput  = props => {
+
+    return (
+        <form>
+        <input type="text" placeholder= "Add comment...."/>
+        </form>
+    );
+};
+
+export default CommentInput;
