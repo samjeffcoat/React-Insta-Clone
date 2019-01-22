a5205318e1e9da8a2a3711d3b0a073fc7a83c94b
samjeffcoat <samjeffcoat@gmail.com>
2019-01-22 14:49:54 -0600

    Set posts to emply array in constructor. Set state in componentDidMount to post: Dummy Data

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -4,12 +4,16 @@ import SearchBar from "./components/SearchBar/SearchBar";
 import PostContainer from "./components/PostContainer/PostContainer";
 import dummyData from "./dummy-data";
 
-class App extends Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      posts: dummyData
+      posts: []
     };
+  }
+  componentDidMount() {
+    console.log("CDM is working");
+    this.setState({ posts: dummyData });
   }
   render() {
     console.log(dummyData);
