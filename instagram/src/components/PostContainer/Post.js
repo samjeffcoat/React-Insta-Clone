343b94d1962e1927b528d6f57673125f0ec00b27
samjeffcoat <samjeffcoat@gmail.com>
2019-01-23 09:40:08 -0600

    likes are working, need to make heart smaller

--- instagram/src/components/PostContainer/Post.js
+++ instagram/src/components/PostContainer/Post.js
@@ -1,14 +1,20 @@
 import React, { Component } from "react";
 import PostHeader from "./PostHeader";
 import CommentSection from "../CommentSection/CommentSection";
+import Heart from "../../photos/heart.svg";
 
 class Post extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      posts: props.posts,
       likes: props.post.likes
     };
   }
+  incrementLikes = () => {
+    let likes = this.state.likes + 1;
+    this.setState({ likes });
+  };
 
   render() {
     return (
@@ -25,6 +31,10 @@ class Post extends React.Component {
           />
         </div>
         <CommentSection comments={this.props.post.comments} />
+        <div>
+          likes= {this.state.likes}
+          <img onClick={this.incrementLikes} src={Heart} alt="likes" />
+        </div>
       </div>
     );
   }
